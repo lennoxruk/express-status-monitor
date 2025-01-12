01e35a34984c7455d79bde58ddc7fcc8f76d793b
lennox <lennox@noreply.com>
2025-01-12 12:11:29 +0000

    change examples tester to use node-fetch-native instead of axios

--- examples/tester.js
+++ examples/tester.js
@@ -1,13 +1,19 @@
-const axios = require("axios");
+const fetch = require('node-fetch-native');
 
 const port = 3000;
 const interval = 50;
 const requestUrl = `http://0.0.0.0:${port}/return-status/`;
 
+/**
+ * Makes a random request to the server at a random interval
+ *
+ * @returns {undefined}
+ */
 const makeDummyCall = () =>
-  setTimeout(() => {
+  setTimeout(async () => {
     const code = 200 + Math.random() * 399;
-    axios.get(`${requestUrl}${code}`).catch(() => {});
+
+    await fetch(`${requestUrl}${code}`).catch(() => {});
 
     makeDummyCall();
   }, interval);
